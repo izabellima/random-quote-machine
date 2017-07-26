0f0f587e39eb7a68e199330eb5153428d569546d
Izabel Lima <belica18@gmail.com>
2017-07-25 22:15:36 -0300

    CSS Style. JS to load new quote working

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
-  $(document).ready(function() {
-
-    $("#getQuote").on("click", function(){
-      $.getJSON("/json/cats.json", function(json) {
-        $(".quote").html(JSON.stringify(json));
-      });
+$(document).ready(function() {
+  $("#getQuote").on("click", function(){
+    $.ajax({
+      type: 'GET',
+      dataType: 'json',
+      url: 'http://quotes.rest/qod.json',
+      success: function(data){
+        $(".quote").html("<h1>" + data.contents.quotes[0].quote + "</h1><h5>" + data.contents.quotes[0].author + "</h5>");
+      }
     });
-
   });
+});
